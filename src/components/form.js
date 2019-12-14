8b81915943e3b16893a80007b8f4551a6096a70b
david-mateogit <davidmateo@outlook.com>
2019-12-14 10:16:45 -0400

    setup create and update

---
+++ src/components/form.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import styles from './form.module.css';
+
+const Form = ({ reloadTodos }) => {
+  const [text, setText] = useState('');
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    if (text === '') return;
+
+    await axios.post('/api/create-todo', { text });
+    setText('');
+    reloadTodos();
+  };
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <label className={styles.label} htmlFor="todo">
+        Add a Todo
+        <input
+          name="todo"
+          type="text"
+          className={styles.input}
+          value={text}
+          onChange={event => setText(event.target.value)}
+        />
+      </label>
+      <button type="button" className={styles.button}>
+        Save Todo
+      </button>
+    </form>
+  );
+};
+
+export default Form;
